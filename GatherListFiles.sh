9997c98b10a9fb2d8a7026f2888f32b9d6e67a10
SlowFeiXu <x2603509554@163.com>
2021-11-27 22:35:09 +0800

    批量收集散落目录

---
+++ GatherListFiles.sh
@@ -0,0 +1,28 @@
+#！/bin/bash
+#-------------------------------------
+#	dest: 移动文件中执行目录项到目标位置
+#	auth: SlowFeiXu
+# 	date: 22:28 2011/11/27
+#-------------------------------------
+
+source_file='/home/xufei/function'
+file_directory='/home/xufei/function'
+file_dest='/home/xufei/function/tmp_test'
+
+`sed -i "s/'\r'/''/g" ${file_dest}`
+
+if [[ -d ${file_dest} ]]
+then
+	`mkdir -p ${file_dest}`
+fi
+
+while read line
+do
+	
+	for file_path in `find ${file_directory} -name ${line}`
+	do
+		`cp -r ${file_path} ${file_dest}`
+		echo "copy ${file_name} to ${file_dest}"
+	done
+
+done < ${source_file}
